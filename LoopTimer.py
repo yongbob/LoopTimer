9c9e3a11b8d65ee92d0047fa0c9c37124502a552
yongbob <44330608+yongbob@users.noreply.github.com>
2018-11-02 20:47:19 +0800

    Update LoopTimer.py

--- LoopTimer.py
+++ LoopTimer.py
@@ -1,6 +1,6 @@
 import threading
 import time
-
+count = 0
 class LoopTimer(threading.Timer):
     '''Call a function after a specified number of seconds:
             t = LoopTimer(30.0, f, args=[], kwargs={})
@@ -9,30 +9,61 @@ class LoopTimer(threading.Timer):
     '''
     def __init__(self, interval, function, *args, **kwargs):
         threading.Timer.__init__(self,interval, function, args, kwargs)
+        self.start_time=time.time()
+        self.__resume = threading.Event()
+        self.__resume.set()
+        
+    def pause(self):
+        self.__resume.clear()
+    
+    def resume(self):
+        self.__resume.set()
+    def stop(self):
+        self.__resume.set()
+        super().cancel()
         
     def run(self):
         while True:
+            print("timer: ",time.time()-self.start_time)
+            self.function(*self.args, **self.kwargs)
             self.finished.wait(self.interval)
+            global count
+            count =count + 1
+            print(count)
+            if not self.__resume.is_set():
+                self.__resume.wait()
             if self.finished.is_set():
                 self.finished.set()
                 break
-            self.function(*self.args, **self.kwargs)
 
+   
 if __name__ == '__main__': 
  
- 
-    def fun_timer(*args,**kwargs):
-        print('The current number of threads is: %s' % threading.active_count())
-        print('thread name:',threading.current_thread())
-        print(*args)
-    
-    t1 = LoopTimer(2,fun_timer,'=== first timer')
-    t1.start()
-    time.sleep(5)
-    t1.cancel()
+    def sayhello(a):
+        print("hello: "+str(a))
 
-    t2= LoopTimer(1,fun_timer,'~~~second timer')
-    t2.start()
+    def fun_timer(*args,**kwargs):
+        for each in args[0]:
+            sayhello(each)
+              
+  
+    seed1=[]
+    for i in range(3):
+        seed1.append(i)
+        
+    t1 = LoopTimer(1,fun_timer,seed1)
+    t1.start()
+    
     time.sleep(5)
-    t2.cancel()
+    print("pause=====")
+    t1.pause()
+    time.sleep(3)
+    print("resume===")
+    t1.resume()
+    time.sleep(5)
+    t1.stop()
+    
+    
     print("end")
+ 
+
